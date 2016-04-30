f86d3b52dd760ba93e502055bdda68288d70249e
Joshua Reibert <joshua.reibert@gmail.com>
2016-04-30 15:20:36 +0200

    Format code

--- src/library/SOMScopeGuard.cpp
+++ src/library/SOMScopeGuard.cpp
@@ -1,30 +1,30 @@
 #include "SOMScopeGuard.hpp"
 
-
 /*
-This function constructs the scope guard and sets the function for it to call when it is deleted or goes out of scope.  The function argument must not throw exceptions.
+This function constructs the scope guard and sets the function for it to call
+when it is deleted or goes out of scope.  The function argument must not throw
+exceptions.
 @param inputFunction: The function to call when the object goes out of scope
 */
-SOMScopeGuard::SOMScopeGuard(std::function<void()> inputFunction) : functionToCall(inputFunction)
-{
-   scopeGuardHasBeenDismissed = false;
+SOMScopeGuard::SOMScopeGuard(std::function<void()> inputFunction)
+		: functionToCall(inputFunction) {
+	scopeGuardHasBeenDismissed = false;
 }
 
 /*
-This function tells the scope guard that it should not call the function it was given when it goes out of scope.
+This function tells the scope guard that it should not call the function it was
+given when it goes out of scope.
 */
-void SOMScopeGuard::dismiss()
-{
-   scopeGuardHasBeenDismissed = true;
+void SOMScopeGuard::dismiss() {
+	scopeGuardHasBeenDismissed = true;
 }
 
 /*
-This destructor cleans up the object and calls the function that it was given on its creation.
+This destructor cleans up the object and calls the function that it was given on
+its creation.
 */
-SOMScopeGuard::~SOMScopeGuard() noexcept
-{
-   if(!scopeGuardHasBeenDismissed)
-   {
-      functionToCall();
-   }
+SOMScopeGuard::~SOMScopeGuard() noexcept {
+	if (!scopeGuardHasBeenDismissed) {
+		functionToCall();
+	}
 }
